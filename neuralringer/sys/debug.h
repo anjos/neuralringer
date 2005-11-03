c799d81be4e20159fa770be8078aa7c2355ebc1e
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2005-11-03 11:37:25 +0000

    Better option parsing and debug messages
    git-svn-id: file:///home/andreanjos/svn/andre_phd/neuralringer/trunk@298 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- neuralringer/sys/debug.h
+++ neuralringer/sys/debug.h
@@ -16,10 +16,27 @@
 #include <iostream>
 
 /**
+ * By default, switch off the debug output if nothing is said
+ */
+#ifndef RINGER_DEBUG
+#define RINGER_DEBUG 0
+#endif
+
+namespace sys {
+
+  /**
+   * Defines if I should or not print the debug messages to std::cout
+   *
+   * @param level The level that has to be checked
+   */
+  bool nr_print_debug(unsigned int level);
+}
+
+/**
  * Highest level debugging output
  */
 #if (RINGER_DEBUG>=1)
-#define RINGER_DEBUG1(m) std::cout << " [DBG1] (" \
+#define RINGER_DEBUG1(m) if (sys::nr_print_debug(1)) std::cout << " [DBG1] (" \
                                  << "/" << __FILE__ << "+" << __LINE__ \
                                  << ") " << m << std::endl
 #else
@@ -30,7 +47,7 @@
  * Middle level debugging output
  */
 #if (RINGER_DEBUG>=2)
-#define RINGER_DEBUG2(m) std::cout << " [DBG2] (" \
+#define RINGER_DEBUG2(m) if (sys::nr_print_debug(2)) std::cout << " [DBG2] (" \
                                  << "/" << __FILE__ << "+" << __LINE__ \
                                  << ") " << m << std::endl
 #else
@@ -41,7 +58,7 @@
  * Lower level debugging output
  */
 #if (RINGER_DEBUG>=3)
-#define RINGER_DEBUG3(m) std::cout << " [DBG3] (" \
+#define RINGER_DEBUG3(m) if (sys::nr_print_debug(3)) std::cout << " [DBG3] (" \
                                  << "/" << __FILE__ << "+" << __LINE__ \
                                  << ") " << m << std::endl
 #else
