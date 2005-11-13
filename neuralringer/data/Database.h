16a654c9f5d9891fd67a1ba9f5fb859f5aa15bf4
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2005-11-13 19:19:33 +0000

    New template-specialized functionality
    git-svn-id: file:///home/andreanjos/svn/andre_phd/neuralringer/trunk@313 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- neuralringer/data/Database.h
+++ neuralringer/data/Database.h
@@ -22,6 +22,7 @@
 #include "sys/debug.h"
 #include "sys/XMLProcessor.h"
 #include "data/Ensemble.h"
+#include "data/RoIPatternSet.h"
 #include <cmath>
 
 namespace data {
@@ -447,18 +448,141 @@ bool data::Database<TSet>::save (const std::string& filename)
   return true;
 }
 
+namespace data {
+
+  /**
+   * Some hackish way to emulate partial template specialization for the merge
+   * functionality on data::Database
+   *
+   * @see data::Database::merge()
+   * 
+   * @param dest The destination set
+   * @param source The source of data information that has to be merged
+   */
+  template <class T>
+  void merge_sets (T& dest, const std::map<std::string, T*>& source)
+  {
+    bool init = false;
+    for (typename std::map<std::string, T*>::const_iterator
+	   it = source.begin(); it != source.end(); ++it) {
+      if (!init) {
+	dest = *it->second;
+	init = true;
+    }
+      else dest.merge(*it->second);
+    }
+  }
+
+  /**
+   * Some hackish way to emulate partial template specialization for the merge
+   * functionality on data::Database. This is the data::RoIPatternSet
+   * specialization.
+   *
+   * @see data::Database::merge()
+   * 
+   * @param dest The destination set
+   * @param source The source of data information that has to be merged
+   */
+  template <> void merge_sets<data::RoIPatternSet> 
+  (data::RoIPatternSet& dest, 
+   const std::map<std::string, data::RoIPatternSet*>& source);
+
+}
+
 template <class TSet>
 void data::Database<TSet>::merge (TSet& ps) const
 {
-  bool init = false;
-  for (typename std::map<std::string, TSet*>::const_iterator
-	 it = m_data.begin(); it != m_data.end(); ++it) {
-    if (!init) {
-      ps = *it->second;
-      init = true;
+  data::merge_sets<TSet>(ps, m_data);
+}
+
+namespace data {
+
+  /**
+   * Some hackish way to emulate partial template specialization for the
+   * merge_target() functionality on data::Database.
+   *
+   * @see data::Database::merge_target()
+   * 
+   * @param minimal Do we use "minimal" representation? A value of
+   * <code>false</code> imples we use the "normal" representation.
+   * @param min The minimum value the reader can interpret (neuron off).
+   * @param max The maximum value the reader can interpret (neuron on).
+   * @param target The target PatternSet according to the parameters before
+   * @param source The source of data information that has to be merged
+   */
+  template <class T> void merge_target_sets
+  (const bool minimal, const double& min, const double& max,
+   T& target, const std::map<std::string, T*>& source)
+  {
+    //The next test probably characterizes this should be split in two
+    //functions, but let's leave it this way for the time being
+    if (minimal) {
+      //Condense the targets to the minimal binary set possible
+      size_t total_size = 0;
+      for (typename std::map<std::string, T*>::const_iterator
+	     it = source.begin(); it != source.end(); ++it)
+	total_size += it->second->size();
+      size_t next_power_of_2 = lrint(std::ceil(log2(source.size())));
+      T my_target(total_size, next_power_of_2, min);
+      size_t class_counter=0;
+      size_t pattern_counter=0;
+      for (typename std::map<std::string, T*>::const_iterator
+	     it = source.begin(); it != source.end(); ++it) {
+	for (size_t j=0; j<it->second->size(); ++j) {
+	  data::Pattern curr_target = make_target(class_counter, 
+						  next_power_of_2,
+						  min, max);
+	  my_target.set_pattern(pattern_counter, curr_target);
+	  ++pattern_counter;
+	}
+	++class_counter;
+      }
+      target = my_target;
     }
-    else ps.merge(*it->second);
+    else {
+      if (source.size() >= 1) {
+	T tmp(source.begin()->second->size(), source.size(), min);
+	data::Ensemble tmpe(source.begin()->second->size(), max);
+	tmp.set_ensemble(0, tmpe);
+	target = tmp;
+      }
+      if (source.size() > 1) {
+	size_t i=1;
+	bool init = false;
+	for (typename std::map<std::string, T*>::const_iterator
+	       it = source.begin(); it != source.end(); ++it) {
+	  T tmp(it->second->size(), source.size(), min);
+	  data::Ensemble tmpe(it->second->size(), max);
+	  tmp.set_ensemble(i++, tmpe);
+	  if (!init) {
+	    target = tmp;
+	    init = true;
+	  }
+	  else target.merge(tmp);
+	}
+      }
+    }
   }
+
+  /**
+   * Some hackish way to emulate partial template specialization for the
+   * merge_target() functionality on data::Database. This is the
+   * data::RoIPatternSet specialization.
+   *
+   * @see data::Database::merge_target()
+   * 
+   * @param minimal Do we use "minimal" representation? A value of
+   * <code>false</code> imples we use the "normal" representation.
+   * @param min The minimum value the reader can interpret (neuron off).
+   * @param max The maximum value the reader can interpret (neuron on).
+   * @param target The target PatternSet according to the parameters before
+   * @param source The source of data information that has to be merged
+   */
+  template <> void merge_target_sets<data::RoIPatternSet>
+  (const bool minimal, const double& min, const double& max, 
+   data::RoIPatternSet& target, 
+   const std::map<std::string, data::RoIPatternSet*>& source);
+
 }
 
 template <class TSet>
@@ -467,54 +591,7 @@ void data::Database<TSet>::merge_target (const bool minimal,
 					 const double& max,
 					 TSet& target) const
 {
-  //The next test probably characterizes this should be split in two
-  //functions, but let's leave it this way for the time being
-  if (minimal) {
-    //Condense the targets to the minimal binary set possible
-    size_t total_size = 0;
-    for (typename std::map<std::string, TSet*>::const_iterator
-	   it = m_data.begin(); it != m_data.end(); ++it)
-      total_size += it->second->size();
-    size_t next_power_of_2 = lrint(std::ceil(log2(m_data.size())));
-    TSet my_target(total_size, next_power_of_2, min);
-    size_t class_counter=0;
-    size_t pattern_counter=0;
-    for (typename std::map<std::string, TSet*>::const_iterator
-	   it = m_data.begin(); it != m_data.end(); ++it) {
-      for (size_t j=0; j<it->second->size(); ++j) {
-	data::Pattern curr_target = make_target(class_counter, 
-						next_power_of_2,
-						min, max);
-	my_target.set_pattern(pattern_counter, curr_target);
-	++pattern_counter;
-      }
-      ++class_counter;
-    }
-    target = my_target;
-  }
-  else {
-    if (m_data.size() >= 1) {
-      TSet tmp(m_data.begin()->second->size(), m_data.size(), min);
-      data::Ensemble tmpe(m_data.begin()->second->size(), max);
-      tmp.set_ensemble(0, tmpe);
-      target = tmp;
-    }
-    if (m_data.size() > 1) {
-      size_t i=1;
-      bool init = false;
-      for (typename std::map<std::string, TSet*>::const_iterator
-	     it = m_data.begin(); it != m_data.end(); ++it) {
-	TSet tmp(it->second->size(), m_data.size(), min);
-	data::Ensemble tmpe(it->second->size(), max);
-	tmp.set_ensemble(i++, tmpe);
-	if (!init) {
-	  target = tmp;
-	  init = true;
-	}
-	else target.merge(tmp);
-      }
-    }
-  }
+  data::merge_target_sets<TSet>(minimal, min, max, target, m_data);
 }
 
 template <class TSet>
