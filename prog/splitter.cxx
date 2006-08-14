695ede6ba2b965264814c823b3fb822345ca414c
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2006-08-14 16:28:22 +0000

    Allow splitter to make energy cuts
    git-svn-id: file:///home/andreanjos/svn/andre_phd/neuralringer/trunk@363 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- prog/splitter.cxx
+++ prog/splitter.cxx
@@ -16,6 +16,7 @@
 #include "sys/util.h"
 #include "sys/OptParser.h"
 #include "data/Database.h"
+#include "data/SumExtractor.h"
 #include <iostream>
 #include <cstdio>
 #include <cmath>
@@ -26,6 +27,7 @@ typedef struct param_t {
   std::string out1; ///< first output database
   std::string out2; ///< second output database
   double percentage; ///< the splitting percentage
+  double energy_cut; ///< at which eT to cut
 } param_t;
 
 /**
@@ -39,6 +41,10 @@ bool checkopt (const param_t& par, sys::Reporter& reporter)
   if (par.percentage <= -1.0 || par.percentage >= 1.0) {
     RINGER_DEBUG1("Trying to set the training percentage to "<< par.percentage);
     throw RINGER_EXCEPTION("This value should be between (-1,1)");
+  }
+  if (par.energy_cut < 0.0) {
+    RINGER_DEBUG1("Trying to set the energy threshold to "<< par.energy_cut);
+    throw RINGER_EXCEPTION("This value should be between (0,Inf)");
   }
   if (!par.db.size()) {
     RINGER_DEBUG1("No DB file given! Throwing...");
@@ -63,12 +69,15 @@ int main (int argc, char** argv)
 {
   sys::Reporter reporter("local");
 
-  param_t par = { "", "", "", 0.5 };
+  param_t par = { "", "", "", 0.5, 170000 };
   sys::OptParser opt_parser(argv[0]);
 
   opt_parser.add_option
     ("train-percentage", 'a', par.percentage,
      "how much of the database to use for first part of the split");
+  opt_parser.add_option
+    ("energy-cut", 'c', par.energy_cut,
+     "energy threshold to cut events");
   opt_parser.add_option
     ("db", 'd', par.db,
      "location of the database to use for the operation");
@@ -89,6 +98,19 @@ int main (int argc, char** argv)
 
   //loads the DB
   data::Database<data::RoIPatternSet> db(par.db, reporter);
+
+  //execute the cutting operation
+  for (std::map<std::string, data::RoIPatternSet*>::const_iterator
+         it = db.data().begin(); it != db.data().end(); ++it) {
+    for (size_t i = 0; i < it->second->size();) {
+      data::SumExtractor sum;
+      if ( sum(it->second->pattern(i)) > par.energy_cut ) {
+        const_cast<data::RoIPatternSet*>(it->second)->erase_pattern(i);
+      }
+      else ++i;
+    }
+  }
+
   //splits the database in train and test
   data::Database<data::RoIPatternSet>* traindb;
   data::Database<data::RoIPatternSet>* testdb;
